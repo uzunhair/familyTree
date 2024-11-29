10a4701319cdfb34ad1f99ae501cf4f65f8aca05
Uzunhair <uzunhair@yandex.ru>
2024-11-29 15:23:23 +0300

    Добавил настройки

--- frontend/src/shared/ui/TextInput/TextInput.tsx
+++ frontend/src/shared/ui/TextInput/TextInput.tsx
@@ -23,25 +23,38 @@ export const TextInput = forwardRef<HTMLInputElement, TTextInput>(({ label, plac
     <div className={className}>
       {label && (
         <label
-          htmlFor={id}
-          className="inline-block mb-2 group-[.form-inline]:mb-2 group-[.form-inline]:sm:mb-0 group-[.form-inline]:sm:mr-5 group-[.form-inline]:sm:text-right"
+          htmlFor={`textField${id}`}
+          className="inline-block mb-2"
         >
           {label}
         </label>
       )}
       <input
         {...props}
-        id={id}
+        id={`textField${id}`}
         type="text"
         placeholder={placeholder}
         ref={ref}
         value={value}
         onChange={handleChange}
-        className="disabled:bg-slate-100 disabled:cursor-not-allowed dark:disabled:bg-darkmode-800/50 dark:disabled:border-transparent [&[readonly]]:bg-slate-100 [&[readonly]]:cursor-not-allowed [&[readonly]]:dark:bg-darkmode-800/50 [&[readonly]]:dark:border-transparent transition duration-200 ease-in-out w-full text-sm border-slate-200 shadow-sm rounded-md placeholder:text-slate-400/90 focus:ring-4 focus:ring-primary focus:ring-opacity-20 focus:border-primary focus:border-opacity-40 dark:bg-darkmode-800 dark:border-transparent dark:focus:ring-slate-700 dark:focus:ring-opacity-50 dark:placeholder:text-slate-500/80 group-[.form-inline]:flex-1 group-[.input-group]:rounded-none group-[.input-group]:[&:not(:first-child)]:border-l-transparent group-[.input-group]:first:rounded-l group-[.input-group]:last:rounded-r group-[.input-group]:z-10"
-      />
+        className="
+          transition duration-200
+          ease-in-out
+          w-full
+          text-sm
+          border-slate-200
+          shadow-sm
+          rounded-md
+          placeholder:text-slate-400/90
+          focus:ring-4
+          focus:ring-primary
+          focus:ring-opacity-20
+          focus:border-primary
+          focus:border-opacity-40
+        "
+     />
     </div>
   );
 });
 
-// Добавляем displayName для отладки
 TextInput.displayName = "TextInput";
