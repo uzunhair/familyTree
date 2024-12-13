0de9b005535b56444b26b5c4d87385b3e5fcbc7c
Uzunhair <uzunhair@yandex.ru>
2024-12-14 00:36:59 +0300

    Добавил вывод календаря при клике на поле

--- frontend/src/shared/ui/DateField/DateField.tsx
+++ frontend/src/shared/ui/DateField/DateField.tsx
@@ -1,20 +1,24 @@
-import {useState} from "react";
-import {MonthSelect} from "src/shared/ui/DateField/MonthSelect";
-import {YearSelect} from "src/shared/ui/DateField/YearSelect";
+import React, { useState, useEffect, useRef } from "react";
+import { MonthSelect } from "src/shared/ui/DateField/MonthSelect";
+import { YearSelect } from "src/shared/ui/DateField/YearSelect";
 import styles from "./DateField.module.scss";
 
 type TProps = {
   label?: string;
   onDateChange?: (value: string) => void;
-}
+};
 
-export const DateField = ({label, onDateChange}: TProps) => {
+export const DateField = ({ label, onDateChange }: TProps) => {
   const date = new Date();
   const fullYear = date.getFullYear();
   const month = date.getMonth();
   const day = date.getDate();
-  const [selectedDate, setSelectedDate] = useState({year: fullYear, month, day});
-  const [inputValue, setInputValue] = useState(`${selectedDate.year}-${(selectedDate.month + 1).toString().padStart(2, "0")}-${selectedDate.day}`);
+  const [selectedDate, setSelectedDate] = useState({ year: fullYear, month, day });
+  const [inputValue, setInputValue] = useState(
+    `${selectedDate.year}-${(selectedDate.month + 1).toString().padStart(2, "0")}-${selectedDate.day}`
+  );
+  const [isCalendarVisible, setIsCalendarVisible] = useState(false);
+  const calendarRef = useRef<HTMLDivElement>(null);
 
   const handleDateChange = (event: any) => {
     const newDate = event.target.value;
@@ -22,8 +26,8 @@ export const DateField = ({label, onDateChange}: TProps) => {
 
     setInputValue(newDate);
 
-    if(year) {
-      setSelectedDate({year, month, day});
+    if (year) {
+      setSelectedDate({ year, month: parseInt(month, 10) - 1, day: parseInt(day, 10) });
     }
     if (onDateChange) {
       onDateChange(newDate);
@@ -40,22 +44,26 @@ export const DateField = ({label, onDateChange}: TProps) => {
 
   const weekName = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
   const weekNumber = [6, 0, 1, 2, 3, 4, 5];
-  const daysOfWeek = Array.from({length: weekNumber[skip]}, (_, i) => i + 1);
+  const daysOfWeek = Array.from({ length: weekNumber[skip] }, (_, i) => i + 1);
 
   const handleMonthChange = (month: number) => {
-    setSelectedDate(prevState => ({...prevState, month}));
+    setSelectedDate((prevState) => ({ ...prevState, month }));
   };
 
   const handleYearChange = (year: number) => {
-    setSelectedDate(prevState => ({...prevState, year}));
+    setSelectedDate((prevState) => ({ ...prevState, year }));
   };
 
   const handleDayChange = (day: number) => {
-    setSelectedDate(prevState => ({...prevState, day}));
+    setSelectedDate((prevState) => ({ ...prevState, day }));
+    setInputValue(
+      `${selectedDate.year}-${(selectedDate.month + 1).toString().padStart(2, "0")}-${day.toString().padStart(2, "0")}`
+    );
+    setIsCalendarVisible(false);
   };
 
   const handleDown = () => {
-    const {month, year} = selectedDate;
+    const { month, year } = selectedDate;
     let newMonth = month - 1;
     let newYear = year;
     if (month < 1) {
@@ -63,11 +71,11 @@ export const DateField = ({label, onDateChange}: TProps) => {
       newYear = year - 1;
     }
 
-    setSelectedDate(prevState => ({...prevState, year: newYear, month: newMonth}));
+    setSelectedDate((prevState) => ({ ...prevState, year: newYear, month: newMonth }));
   };
 
   const handleUp = () => {
-    const {month, year} = selectedDate;
+    const { month, year } = selectedDate;
     let newMonth = month + 1;
     let newYear = year;
     if (month === 11) {
@@ -75,7 +83,7 @@ export const DateField = ({label, onDateChange}: TProps) => {
       newYear = year + 1;
     }
 
-    setSelectedDate(prevState => ({...prevState, year: newYear, month: newMonth}));
+    setSelectedDate((prevState) => ({ ...prevState, year: newYear, month: newMonth }));
   };
 
   const list = [];
@@ -98,6 +106,24 @@ export const DateField = ({label, onDateChange}: TProps) => {
     );
   }
 
+  const handleClickOutside = (event: MouseEvent) => {
+    if (calendarRef.current && !calendarRef.current.contains(event.target as Node)) {
+      setIsCalendarVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isCalendarVisible) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isCalendarVisible]);
+
   return (
     <div className={styles.layout}>
       {label && <label>{label}</label>}
@@ -105,42 +131,52 @@ export const DateField = ({label, onDateChange}: TProps) => {
         type="date"
         value={inputValue}
         onChange={handleDateChange}
+        onFocus={() => setIsCalendarVisible(true)}
         className="
-          transition duration-200
-          ease-in-out
-          w-full
-          text-sm
-          border-slate-200
-          shadow-sm
-          rounded-md
-          placeholder:text-slate-400/90
-          focus:ring-4
-          focus:ring-primary
-          focus:ring-opacity-20
-          focus:border-primary
-          focus:border-opacity-40
-        "/>
-      <div className={styles.calendar}>
-        <div className="flex gap-3 mb-2 items-center">
-          <button type="button" onClick={handleDown} className={styles.upDownBtn}>
-            <svg width="11" height="16" xmlns="http://www.w3.org/2000/svg">
-              <path d="M7.919 0l2.748 2.667L5.333 8l5.334 5.333L7.919 16 0 8z" fillRule="nonzero"></path>
-            </svg>
-          </button>
-          <MonthSelect date={date} onChange={handleMonthChange} value={selectedDate.month}/>
-          <YearSelect fullYear={selectedDate.year} onChange={handleYearChange}/>
-          <button type="button" onClick={handleUp} className={styles.upDownBtn}>
-            <svg width="11" height="16" xmlns="http://www.w3.org/2000/svg">
-              <path d="M2.748 16L0 13.333 5.333 8 0 2.667 2.748 0l7.919 8z" fillRule="nonzero"></path>
-            </svg>
-          </button>
+        transition duration-200
+        ease-in-out
+        w-full
+        text-sm
+        border-slate-200
+        shadow-sm
+        rounded-md
+        placeholder:text-slate-400/90
+        focus:ring-4
+        focus:ring-primary
+        focus:ring-opacity-20
+        focus:border-primary
+        focus:border-opacity-40
+      "
+      />
+      {isCalendarVisible && (
+        <div className={styles.calendar} ref={calendarRef}>
+          <div className="flex gap-3 mb-2 items-center">
+            <button type="button" onClick={handleDown} className={styles.upDownBtn}>
+              <svg width="11" height="16" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7.919 0l2.748 2.667L5.333 8l5.334 5.333L7.919 16 0 8z" fillRule="nonzero"></path>
+              </svg>
+            </button>
+            <MonthSelect date={date} onChange={handleMonthChange} value={selectedDate.month} />
+            <YearSelect fullYear={selectedDate.year} onChange={handleYearChange} />
+            <button type="button" onClick={handleUp} className={styles.upDownBtn}>
+              <svg width="11" height="16" xmlns="http://www.w3.org/2000/svg">
+                <path d="M2.748 16L0 13.333 5.333 8 0 2.667 2.748 0l7.919 8z" fillRule="nonzero"></path>
+              </svg>
+            </button>
+          </div>
+          <div className={styles.days}>
+            {weekName.map((week) => (
+              <div key={week} className={styles.week}>
+                {week}
+              </div>
+            ))}
+            {daysOfWeek.map((item) => (
+              <div key={item} />
+            ))}
+            {list}
+          </div>
         </div>
-        <div className={styles.days}>
-          {weekName.map(week => <div key={week} className={styles.week}>{week}</div>)}
-          {daysOfWeek.map(item => <div key={item}/>)}
-          {list}
-        </div>
-      </div>
+      )}
     </div>
   );
 };
