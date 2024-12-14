c0071ae3d6a8ba447d59d28a2e906058d8d5eced
Uzunhair <uzunhair@yandex.ru>
2024-12-14 13:16:38 +0300

    Добавил настройку связей

--- frontend/src/pages/ForceGraph/ForceGraph.tsx
+++ frontend/src/pages/ForceGraph/ForceGraph.tsx
@@ -1,57 +1,62 @@
 import {useEffect, useState} from "react";
+import {Leva, useControls} from "leva";
 import {ForceGraph2D} from "react-force-graph";
 import {GetNodes} from "../../../wailsjs/go/main/App";
-
-type TNode = {
-  id:         string,
-  name:       string,
-  gender:     string,
-  father:     string,
-  mother:     string,
-  spouse:     string[],
-  friends:    string[],
-  colleagues: string[],
-  familiar:   string[],
-}
-
-type TLinks = {
-  source: string;
-  target: string;
-  relation: string;
-}
+import {TLinks, TNode} from "src/pages/ForceGraph/lib/types/types";
+import {brotherAndSister} from "src/pages/ForceGraph/lib/utils/brotherAndSister";
 
 export const ForceGraph = () => {
   const [nodes, setNodes] = useState<TNode[]>([]);
+  const {nodeLabel, father, mother, brotherAndSisterControl} = useControls({
+    nodeLabel: {
+      value: "name",
+      label: "Заголовок",
+      options: {"ФИО":"name", "ID":"id"},
+    },
+    father: {
+      label: "Отец",
+      value: true
+    },
+    mother: {
+      label: "Мать",
+      value: true
+    },
+    brotherAndSisterControl: {
+      label: "Брат/Сестра",
+      value: true
+    },
+  }, {});
 
   const links = nodes.reduce<TLinks[]>((acc, item) => {
-    if (item?.father) {
+    if (item?.father && father) {
       acc.push({source: item.id, target: item.father, relation: "отец = ребенок"});
     }
-    // if (item?.children) {
-    //   item.children.map((children) => {
-    //     acc.push({source: item.id, target: children, relation: "родитель > ребенок"});
-    //   });
-    // }
-
+    if (item?.mother && mother) {
+      acc.push({source: item.id, target: item.mother, relation: "мать = ребенок"});
+    }
     return acc;
   }, []);
 
+  const brotherAndSisterLinks = brotherAndSisterControl ? brotherAndSister({ nodes }) : [];
+
   const date = {
     "nodes": nodes,
-    "links": links
+    "links": [...links, ...brotherAndSisterLinks]
   };
   
   useEffect(() => {
     GetNodes().then((result) => {
-      console.log("res", result);
+      // console.log("res", result);
       setNodes(result);
     });
   }, []);
 
   return (
     <div>
+      <Leva />
       <ForceGraph2D
         graphData={date}
+        nodeLabel={nodeLabel}
       />
     </div>
   );
---
+++ frontend/src/pages/ForceGraph/lib/utils/brotherAndSister.ts
@@ -0,0 +1,37 @@
+import {TLinks, TNode} from "src/pages/ForceGraph/lib/types/types";
+
+type TProps = {
+  nodes: TNode[]
+}
+
+// Создаем массив всех детей для каждого родителя
+export const brotherAndSister = ({ nodes }:TProps) => {
+  const links: TLinks[] = []
+
+  const childrenByParent: { [key: string]: string[] } = {};
+
+  nodes.forEach(node => {
+    if (node.father) {
+      if (!childrenByParent[node.father]) {
+        childrenByParent[node.father] = [];
+      }
+      childrenByParent[node.father].push(node.id);
+    }
+    if (node.mother) {
+      if (!childrenByParent[node.mother]) {
+        childrenByParent[node.mother] = [];
+      }
+      childrenByParent[node.mother].push(node.id);
+    }
+  });
+
+  Object.values(childrenByParent).forEach(siblings => {
+    siblings.forEach((sibling, index) => {
+      for (let i = index + 1; i < siblings.length; i++) {
+        links.push({ source: sibling, target: siblings[i], relation: "брат/сестра" });
+      }
+    });
+  });
+
+  return links
+}
